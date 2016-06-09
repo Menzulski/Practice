bc621cc07651b9f617ef56657ad24847291cc2e5
Menzulski <menzulski@gmail.com>
2016-06-09 13:50:19 +0300

    precfs practice

---
+++ LinuxDev/Procfs/pract_0.c
@@ -0,0 +1,35 @@
+//Get CPU model name
+#include <stdio.h>
+#include <string.h>
+
+void getCPUInfo()
+{
+	FILE* fp;
+	char buffer[1024];
+	size_t bytesRead;
+	char* match;
+	char modelName[100];
+
+	//load file /proc/cpuinfo to buffer
+	fp = fopen("/proc/cpuinfo", "r");
+	bytesRead = fread(buffer, 1, sizeof(buffer), fp);
+	fclose(fp);
+
+	if(bytesRead == 0 /*|| bytesRead == sizeof(buffer)*/){
+		printf("UPS\n");
+		printf("%lu bytes\n",bytesRead);
+		return 0;
+	}
+
+	buffer[bytesRead] = '\0';
+	
+	match = strstr(buffer, "model name");
+	sscanf(match, "model name : %[^\n]", modelName);
+	printf("%s\n", modelName);
+}
+
+int main()
+{
+	getCPUInfo();
+	return 0;
+}
